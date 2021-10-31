44de5528015850ff3dcfbe9275ecfcc5946f069a
Joshua M. Boniface <joshua@boniface.me>
2021-10-31 02:42:20 -0400

    Add License notification

--- bbuilder/lib/worker.py
+++ bbuilder/lib/worker.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+# worker.py - Basic Builder Celery worker functions
+# Part of the Basic Builder system
+#
+#    Copyright (C) 2021 Joshua M. Boniface <joshua@boniface.me>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###############################################################################
+
 import contextlib
 import os
 import json
